8f2380a47eabde32e6e3c80ce4f078aba236c9b3
dharaneesh-sm <sdharaneesh007@gmail.com>
2024-08-08 21:44:46 +0530

    Deployment

--- Frontend/src/context/globalContext.js
+++ Frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:4000/api/v1/";
+const BASE_URL = "https://spendwise-upar.onrender.com";
 
 
 const GlobalContext = React.createContext()
