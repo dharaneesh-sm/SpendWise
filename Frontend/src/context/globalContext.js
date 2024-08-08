b69167be2c38e69e870895e348389a507037daf0
Dharaneesh S <107857343+dharaneesh-sm@users.noreply.github.com>
2024-08-08 22:21:42 +0530

    Update globalContext.js

--- Frontend/src/context/globalContext.js
+++ Frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "https://spendwise-upar.onrender.com";
+const BASE_URL = "https://spendwise-upar.onrender.com/api/v1";
 
 
 const GlobalContext = React.createContext()
